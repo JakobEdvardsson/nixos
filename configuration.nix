a0fbb7d11f978828e980f5489e3740dd5f195bc1
Jakob Edvardsson <jakob.edvardsson@outlook.com>
2023-12-17 15:56:39 +0100

    More Hyprland settings for Nvidia GPU

--- configuration.nix
+++ configuration.nix
@@ -51,7 +51,26 @@
   services.xserver.desktopManager.gnome.enable = true;
 
   # Hyprland
-  programs.hyprland.enable = true;
+  programs.hyprland = {    
+    enable = true;
+    xwayland.enable = true;    
+    nvidiaPatches = true; 
+  };
+
+  environment.sessionVariables = {
+    #If your cursor becomes invisible
+    WLR_NO_HARDWARE_CURSORS = "1";
+    #Hint electron apps to use wayland
+    NIXOS_OZONE_WL = "1";
+  };
+
+  hardware = {
+    #Opengl
+    opengl.enable = true;
+
+    #Most wayland compositors need this
+    nvidia.modesetting.enable = true;
+  };
 
 
   # Configure keymap in X11
